ea37c1b385cedc52de04901209a24740e9f2e0fe
Justin10967631 <103071760+Justin10967631@users.noreply.github.com>
2024-06-02 23:53:03 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int regularPrice = 10;
+            const int discountedPrice = 7;
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? discountedPrice : regularPrice;
+
+            Console.WriteLine("Ticket price: GHC" + ticketPrice);
+        }
+    }
+}
